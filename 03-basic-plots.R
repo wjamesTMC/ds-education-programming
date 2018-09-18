f21d4886c35727a86f601b784df6ef0d1d2e8904
wjamesTMC <jamesw@csps.com>
2018-09-18 16:59:37 -0400

    Final commit of the day

--- 03-basic-plots.R
+++ 03-basic-plots.R
@@ -32,3 +32,11 @@ hist(population_in_millions)
 # Create a boxplot of state populations by region for the murders dataset
 boxplot(population~region, data = murders)
 
+# Boxplots are even more useful when we want to quickly compare two or more
+# distributions. For example, here are the heights for men and women:
+     
+heights %>% ggplot(aes(x=sex, y=height, fill=sex)) +
+geom_boxplot()
+
+heights %>% filter(sex=="Female") %>% top_n(5, desc(height)) %>% .$height
+#> [1] 51 53 55 52 52
